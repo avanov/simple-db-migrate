e4f0117b5f9981cea02616650f88324ff1dc9e5e
Maxim Avanov <maxim.avanov@gmail.com>
2016-09-27 18:03:07 +0300

    Add Django app config to make it 1.9+ compatible.

---
+++ simple_db_migrate/db_migrate/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'simple_db_migrate.db_migrate.apps.Config'
---
+++ simple_db_migrate/db_migrate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Config(AppConfig):
+    name = 'simple_db_migrate'
+    verbose_name = "Simple DB Migrate"
